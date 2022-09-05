1c07b8037581583e35de5ce6c59424b35f645f4d
Zhivko Lefterov <zlefterov101@gmail.com>
2022-09-05 10:17:54 +0300

    moved the action plan comments in one place

--- password-generator.py
+++ password-generator.py
@@ -1,32 +1,31 @@
 #python password generator
+
+#store all characters in a list
+#ask the user to add password length
+#shuffle characters via random.shuffle method
+#initialize an emptry list to store the password
+#Loop that iterates length times
+#-pick random characters from random.choice
+#-append the random characters to the password
+#shuffle the password list to make it more random
+#convert password list to string
+#print the generated password
+
 import string
 import random
 
-#store all characters in a list
 characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
-
-#ask the user to add password length
 def generate_random_password():
     length = int(input("How long the generated password should be: "))
-
-    random.shuffle(characters) #shuffle characters via random.shuffle method
-
-    password = [] #initialize an emptry list to store the password
-
-    for i in range(length): #Loop that iterates length times.
+    random.shuffle(characters) 
+    password = [] 
+    for i in range(length): 
         if i < 5: 
             print("Are you sure that short password will be good for you?")
     else i > 8:
-        print("That's a good start for a strong password! Good job!")
-            
-
-    #-pick random characters from random.choice
-    #-append the random characters to the password
+        print("That's a good start for a strong password! Good job!")            
         password.append(random.choice(characters))
-
-    random.shuffle(password) #shuffle the password list to make it more random
-
-    #convert password list to string
-    #print the generated password
+    random.shuffle(password)     
     print(''.join(password))
+    
 generate_random_password()
